300ab523e3e3dc774baf9a5277e094204b14d206
cezium55 <garvitgaur2006@gmail.com>
2026-09-02 01:12:05 +0530

    feat: add CLI argument parser and express server setup

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+const args = process.argv;
+
+let port = 3000;
+let origin = '';
+let clearCache = false;
+
+for ( let i = 2; i< args.length; i++){
+    if(args[i] === '---port'){
+        port = Number(args[i+1]);
+    }
+
+
+    if (args[i] === '--origin'){
+    origin = args[i+1];
+    }
+
+
+    if (args[i] === '---clear-cache'){
+    clearCache = true;
+    }
+}
+
+if(clearCache){
+    console.log('clearing the cache../');
+    process.exit(0);
+}
+
+if(origin === ''){
+    console.log('error:you must provide an --origin URL!');
+    process.exit(1);
+}
+
+
+app.listen(port, function(){
+    console.log('sucess! the proxy server is running on port:'+port);
+    console.log('it will forward requests to: '+origin);
+})
+
+
+
+
