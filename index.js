e5b3d2fa3a7f6bfee146b77b2bbe4b76ec81e3a2
cezium55 <garvitgaur2006@gmail.com>
2026-09-02 23:55:45 +0530

    added request,response using axios

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+const axios = require('axios');
 const express = require('express');
 const app = express();
 
 const args = process.argv;
-
 let port = 3000;
 let origin = '';
 let clearCache = false;
 
 for ( let i = 2; i< args.length; i++){
-    if(args[i] === '---port'){
+    if(args[i] === '--port'){
         port = Number(args[i+1]);
     }
 
@@ -18,7 +18,7 @@ for ( let i = 2; i< args.length; i++){
     }
 
 
-    if (args[i] === '---clear-cache'){
+    if (args[i] === '--clear-cache'){
     clearCache = true;
     }
 }
@@ -33,12 +33,20 @@ if(origin === ''){
     process.exit(1);
 }
 
+app.use(async function(req, res){
+    const targetUrl = origin + req.url;
+    console.log('forwarding request to: ',targetUrl);
+        try{
+            const response = await axios.get(targetUrl);
+            res.setHeader('X-Cache', 'MISS');
+            res.json(response.data);
+        }catch(error){
+            res.status(500).json({error: 'failed to fetch data from the origin server'});
+        }
+
+});
 
 app.listen(port, function(){
     console.log('sucess! the proxy server is running on port:'+port);
     console.log('it will forward requests to: '+origin);
 })
-
-
-
-
